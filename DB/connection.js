6f2153427f52c651cdd8049f528babcb252e223f
vaishnavms9656 <vaishnavms9656@gmail.com>
2024-05-20 05:56:01 +0530

    commit

--- DB/connection.js
+++ DB/connection.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const mysql = require('mysql2');
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: 'user_management'
+    database: process.env.DB_NAME
 });
 
 db.connect((err) => {
