66d0687cfdd78ec8d4939368ffc474f325a48427
vaishnavms9656 <vaishnavms9656@gmail.com>
2024-05-20 06:19:52 +0530

    commit

--- DB/connection.js
+++ DB/connection.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
